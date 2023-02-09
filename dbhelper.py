289caf92fbf0468f7463df8ae53e61228b9bea04
Paulo Souza <souzapeus@gmail.com>
2023-02-09 08:35:54 -0300

    ins

--- dbhelper.py
+++ dbhelper.py
@@ -21,7 +21,7 @@ class DBHelper:
 
     def add_table(self, number, owner):
         new_id = self.db.tables.insert_one({"number": number, "owner": owner})
-        return new_id.toString()
+        return new_id.insertedId.toString()
 
     def update_table(self, _id, url, qrc):
         self.db.tables.update_one({"_id": ObjectId(_id)}, {"$set": {"url": url, "qrc": qrc}})
