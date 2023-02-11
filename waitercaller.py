341b891808005b9eea821c2cdfe4507f4f6a0ff7
Paulo Souza <souzapeus@gmail.com>
2023-02-11 18:52:07 -0300

    srt

--- waitercaller.py
+++ waitercaller.py
@@ -159,7 +159,8 @@ def account_createtable():
     form = CreateTableForm(request.form)
     if form.validate():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
-        new_url = BH.shorten_url(config.base_url + "/newrequest/" + str(tableid))
+        #new_url = BH.shorten_url(config.base_url + "/newrequest/" + str(tableid))
+        new_url = f'{config.base_url}/newrequest/{tableid}'
         new_qrc = QH.gen_code(form.tablenumber.data, new_url)
         DB.update_table(tableid, new_url, new_qrc)
         return redirect(url_for('account'))
