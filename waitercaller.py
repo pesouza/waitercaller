bef30f0a49822dc19bd345756c37972ac1a477f7
Paulo Souza <souzapeus@gmail.com>
2023-02-17 16:01:37 -0300

    sdf

--- waitercaller.py
+++ waitercaller.py
@@ -116,7 +116,7 @@ def register():
 
         customer = stripe.Customer.create(
             email=request.form['email'],
-            estabelecimento=request.form['place']
+            metadata-{'estabelecimento': request.form['place']}
         )
 
         salt = PH.get_salt()
@@ -236,63 +236,89 @@ def privacidade():
 def termos():
     return render_template("termos.html")
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    event = None
-    payload = request.data
-    sig_header = request.headers['STRIPE_SIGNATURE']
-
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
+        prices = stripe.Price.list(
+            lookup_keys=[request.form['lookup_key']],
+            expand=['data.product']
         )
-    except ValueError as e:
-        # Invalid payload
-        raise e
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        raise e
 
-    # Handle the event
-    
-    # Handle the event
-    if event['type'] == 'checkout.session.async_payment_failed':
-      session = event['data']['object']
-    elif event['type'] == 'checkout.session.async_payment_succeeded':
-      session = event['data']['object']
-    elif event['type'] == 'checkout.session.completed':
-      session = event['data']['object']
-    elif event['type'] == 'checkout.session.expired':
-      session = event['data']['object']
-    elif event['type'] == 'customer.created':
-      customer = event['data']['object']
-    elif event['type'] == 'customer.deleted':
-      customer = event['data']['object']
-    elif event['type'] == 'customer.updated':
-      customer = event['data']['object']
-    elif event['type'] == 'customer.subscription.created':
-      subscription = event['data']['object']
-    elif event['type'] == 'customer.subscription.deleted':
-      subscription = event['data']['object']
-    elif event['type'] == 'customer.subscription.paused':
-      subscription = event['data']['object']
-    elif event['type'] == 'customer.subscription.resumed':
-      subscription = event['data']['object']
-    elif event['type'] == 'customer.subscription.trial_will_end':
-      subscription = event['data']['object']
-    elif event['type'] == 'customer.subscription.updated':
-      subscription = event['data']['object']
-    elif event['type'] == 'person.created':
-      person = event['data']['object']
-    elif event['type'] == 'person.deleted':
-      person = event['data']['object']
-    elif event['type'] == 'person.updated':
-      person = event['data']['object']
-    # ... handle other event types
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': prices.data[0].id,
+                    'quantity': 1,
+                },
+            ],
+            mode='subscription',
+            success_url=YOUR_DOMAIN +
+            '/success.html?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+        return redirect(checkout_session.url, code=303)
+    except Exception as e:
+        print(e)
+        return "Server error", 500
+
+@app.route('/create-portal-session', methods=['POST'])
+def customer_portal():
+    # For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+    # Typically this is stored alongside the authenticated user in your database.
+    checkout_session_id = request.form.get('session_id')
+    checkout_session = stripe.checkout.Session.retrieve(checkout_session_id)
+
+    # This is the URL to which the customer will be redirected after they are
+    # done managing their billing with the portal.
+    return_url = YOUR_DOMAIN
+
+    portalSession = stripe.billing_portal.Session.create(
+        customer=checkout_session.customer,
+        return_url=return_url,
+    )
+    return redirect(portalSession.url, code=303)
+
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    # Replace this endpoint secret with your endpoint's unique secret
+    # If you are testing with the CLI, find the secret by running 'stripe listen'
+    # If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    # at https://dashboard.stripe.com/webhooks
+    webhook_secret = 'whsec_12345'
+    request_data = json.loads(request.data)
+
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
     else:
-      print('Unhandled event type {}'.format(event['type']))
+        data = request_data['data']
+        event_type = request_data['type']
+    data_object = data['object']
 
-    return jsonify(success=True)
+    print('event ' + event_type)
+
+    if event_type == 'checkout.session.completed':
+        print('🔔 Payment succeeded!')
+    elif event_type == 'customer.subscription.trial_will_end':
+        print('Subscription trial will end')
+    elif event_type == 'customer.subscription.created':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.updated':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.deleted':
+        # handle subscription canceled automatically based
+        # upon your subscription settings. Or if the user cancels it.
+        print('Subscription canceled: %s', event.id)
+
+    return jsonify({'status': 'success'})
 
 
 if __name__ == '__main__':
