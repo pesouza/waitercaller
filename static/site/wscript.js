00a5f9e49c1e8da2be02e92f6e80bbb92d027f15
Paulo Souza <souzapeus@gmail.com>
2023-02-24 19:40:56 -0300

    ZZ

--- static/site/wscript.js
+++ static/site/wscript.js
@@ -154,18 +154,18 @@ function getWaiterRect() {
 }
 
 function setWaiterLose() {
-  waiter.src = "static/site/waiter-lose.png";
+  waiter.src = "../static/site/waiter-lose.png";
 }
 
 function handleRun(delta, speedScale) {
   if (isJumping) {
-    waiter.src = `static/site/waiter-jump.png`;
+    waiter.src = `../static/site/waiter-jump.png`;
     return;
   }
 
   if (currentFrameTime >= FRAME_TIME) {
     waiterFrame = (waiterFrame + 1) % WAITER_FRAME_COUNT;
-    waiter.src = `static/site/waiter-run-${waiterFrame}.png`; /* switch between images to simulate movement */
+    waiter.src = `../static/site/waiter-run-${waiterFrame}.png`; /* switch between images to simulate movement */
     currentFrameTime -= FRAME_TIME;
   }
   currentFrameTime += delta * speedScale;
@@ -230,7 +230,7 @@ function getTableRects() {
 
 function createTable() {
   const table = document.createElement("img");
-  table.src = "static/site/table.png";
+  table.src = "../static/site/table.png";
   table.classList.add("table");
   setCustomProperty(table, "--left", 100);
   game.append(table); 
